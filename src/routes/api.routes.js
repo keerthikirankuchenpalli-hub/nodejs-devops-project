f87e66578cdbcbfee2d09e02429563b44a48a8b3
keerthikirankuchenpalli-hub <keerthikiran.kuchenpalli@bhrish.com>
2026-01-29 11:38:33 +0530

    adding API.routes

---
+++ src/routes/api.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+const controller = require('../controllers/api.controller.js');
+
+router.get('/data', controller.getData);
+
+router.get( '/message', controller.getMessage);
+
+router.post('/login', controller.loginUser);
+
+module.exports = router;  
