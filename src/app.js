8d4fe42e8d360e297d0ddd2459dba6aa43554e8f
keerthikirankuchenpalli-hub <keerthikiran.kuchenpalli@bhrish.com>
2026-01-29 12:31:22 +0530

    fixed errorsin app file

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 
-const config = require('./routes/api.routes');
+const apiRoutes = require('./routes/api.routes');
 
-const loger = require('./middleware/logger.middleware');
+const logger = require('./middleware/logger.middleware');
 
 const app = express();
 
 app.use(express.json());
 app.use(logger);
 app.use('/api', apiRoutes);
+
 
 module.exports = app;
